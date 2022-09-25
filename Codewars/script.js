110b69e7cbf5ad17769a58d469b26575872e79a0
yuliabalkiss <balkisula451@gmail.com>
2022-09-25 19:00:15 +0300

    add file

--- Codewars/script.js
+++ Codewars/script.js
@@ -88,3 +88,16 @@ function giveMeFive(obj) {
     }
     return myArr;
 }
+
+// Training JS #13: Number object and its properties
+
+function whatNumberIsIt(n) {
+    switch (true) {
+        case (n == Number.MAX_VALUE): return "Input number is Number.MAX_VALUE";
+        case (n == Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
+        case (isNaN(n)): return "Input number is Number.NaN";
+        case (n == Number.NEGATIVE_INFINITY): return "Input number is Number.NEGATIVE_INFINITY";
+        case (n == Number.POSITIVE_INFINITY): return "Input number is Number.POSITIVE_INFINITY";
+        default: return "Input number is " + n;
+    }
+}
