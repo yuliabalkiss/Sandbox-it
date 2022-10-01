2a7c2c812f7a13fcc1109192e01ef8bb9ce54bb9
yuliabalkiss <balkisula451@gmail.com>
2022-10-01 17:12:17 +0300

    added the file

--- Codewars/script.js
+++ Codewars/script.js
@@ -151,3 +151,24 @@ function countSheeps(arrayOfSheep) {
     }
     return b
 }
+
+// Switch/Case - Bug Fixing #6
+
+
+function evalObject(value) {
+    var result = 0;
+    switch (value.operation) {
+        case '+': result = value.a + value.b;
+            break;
+        case '-': result = value.a - value.b;
+            break;
+        case '/': result = value.a / value.b;
+            break;
+        case '*': result = value.a * value.b;
+            break;
+        case '%': result = value.a % value.b;
+            break;
+        case '^': result = Math.pow(value.a, value.b);
+    }
+    return result;
+}
