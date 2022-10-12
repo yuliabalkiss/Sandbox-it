a1046f243443a4b9d5dc694832131d15ff4b9034
yuliabalkiss <balkisula451@gmail.com>
2022-10-12 22:44:27 +0300

    added the files

--- Codewars/script.js
+++ Codewars/script.js
@@ -218,3 +218,10 @@ function deleteVowels(str) {
 
 
 }
+
+function howManyDalmatians(numer) {
+    let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+    return (numer <= 10) ? dogs[0] || (numer <= 50) ? dogs[1] || (numer == 101) ? dogs[3] : dogs[2]
+}
+
+
