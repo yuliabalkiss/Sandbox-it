1ae919e0c3906a186dfc2cb8c5aa8184997f6eac
yuliabalkiss <balkisula451@gmail.com>
2022-10-16 23:41:51 +0300

    feat: add file

--- Codewars/script.js
+++ Codewars/script.js
@@ -231,3 +231,14 @@ function filterWords(phrase) {
 
 
 
+// Sum of positive
+
+function positiveSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
