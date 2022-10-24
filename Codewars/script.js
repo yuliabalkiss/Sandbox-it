9adad3650a4eea46a8a3950fb84aabaa798f4dbf
yuliabalkiss <balkisula451@gmail.com>
2022-10-24 22:32:44 +0300

    feat: add file

--- Codewars/script.js
+++ Codewars/script.js
@@ -276,3 +276,22 @@ function whatday(num) {
     }
 }
 
+
+// Calculate average
+function findAverage(array) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+
+    } if (array.length == []) {
+        return 0;
+    } else {
+        return sum / array.length
+    }
+
+}
+
+
+
+
+
