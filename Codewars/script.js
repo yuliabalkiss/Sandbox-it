5583fbbd9125e8ff01b9f76e96257d6c6a391ddb
yuliabalkiss <balkisula451@gmail.com>
2022-11-06 22:43:43 +0300

    feat: add file

--- Codewars/script.js
+++ Codewars/script.js
@@ -343,3 +343,12 @@ function monkeyCount(n) {
     }
     return count;
 }
+
+// Are You Playing Banjo?
+function areYouPlayingBanjo(name) {
+    if (name[0].toLowerCase() == "r") {
+        return name + " plays banjo";
+    } else {
+        return name + " does not play banjo";
+    }
+}
