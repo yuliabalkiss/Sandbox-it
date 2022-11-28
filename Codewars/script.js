7d565e8642689a8703ab82548545b8c45f966270
yuliabalkiss <balkisula451@gmail.com>
2022-11-29 00:04:21 +0300

    change file

--- Codewars/script.js
+++ Codewars/script.js
@@ -469,3 +469,15 @@ var FilterNumbers = function (str) {
 
     return a
 }
+
+// A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future"
+function yourFutureCareer() {
+    let career = Math.random();
+    if (career <= 0.32) {
+        return 'FrontEnd Developer';
+    } else {
+        if (career <= 0.65) {
+            return 'BackEnd Developer';
+        } else return 'Full-Stack Developer';
+    }
+}
